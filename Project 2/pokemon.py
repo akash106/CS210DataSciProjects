993c883b89f784e0bcea4bfc546c11e173033552
zainchalisa <zain.chalisa@gmail.com>
2022-10-18 12:58:42 -0400

    file setup

--- Project 2/pokemon.py
+++ Project 2/pokemon.py
@@ -2,9 +2,26 @@ import math
 import collections
 import csv
 import re
+from tkinter.tix import Select
 
 # git add - A, commit -m"message", push (to add files to REPO)
 # git pull (to get files from REPO)
 
+def findPercentage ():
+    pass
 
+def missingType():
+    pass
 
+def missingVals():
+    pass
+
+def personalityDict():
+    pass
+
+def avgHitPoints():
+    pass
+
+def main():
+    pass
+main()    
