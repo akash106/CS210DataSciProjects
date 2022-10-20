797b53ab0f8775732db430c7357e1546a3489d96
zainchalisa <zain.chalisa@gmail.com>
2022-10-20 01:53:09 -0400

    part 2 of assignmnet 2

---
+++ Project 2/covid.py
@@ -0,0 +1,46 @@
+import math
+import collections
+import csv
+import re
+
+
+def avgAge (file):
+
+
+    # open the write file
+    
+
+    # open the read file (which will be the same as the write file)
+
+        with open(file) as covidResults1:
+            reader = csv.DictReader(covidResults1)
+            for row in reader:
+                
+                if re.search('-', row['age']):
+                    match = re.split(r"\D", row['age'])
+                    sum = 0
+                    length = len(range(int(match[0]), int(match[1])))
+                    if length > 0:
+                        for i in range(int(match[0]), int(match[1])):
+                            sum += i
+                        avg = round(sum/length)
+                        row['age'].replace(row['age'], str(avg))
+                        print(avg)
+                    else:
+                        row['age'].replace(row['age'], str(match[0]))
+                        print(match[0])
+                        
+                
+                   
+
+
+    # iterate through the rows checking the age
+        # use regex .split() and then set those two values as start and end
+        # iterate through that range and find the sum of those values
+        # get out of that loop and repalce the age range with the avg
+    
+    
+
+def main():
+    avgAge('covidTrain.csv')
+main()
