e2032953cacc6e05b1da2b906ceaec21fe5e8cc7
zainchalisa <zain.chalisa@gmail.com>
2022-10-21 00:32:20 -0400

    tfidf file setup ... this could be wrong

---
+++ Project 2/tfidf.py
@@ -0,0 +1,34 @@
+import math
+import collections
+import csv
+import re
+
+
+def clean ():
+    pass
+
+def stopWords():
+    pass
+
+def stemLem():
+    pass
+
+def distinctWords():
+    pass
+
+def termFreq():
+    pass
+
+def invDocFreq():
+    pass
+
+def TFIDFScore():
+    pass
+
+def top5Words():
+    pass
+
+def main():
+    pass
+main()
+
