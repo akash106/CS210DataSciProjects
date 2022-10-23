c9c00b5714841eff9a7e563e6f6eb9399a469515
zainchalisa <zain.chalisa@gmail.com>
2022-10-23 00:48:56 -0400

    idk how to do this regex stuff

--- Project 2/tfidf.py
+++ Project 2/tfidf.py
@@ -4,14 +4,38 @@ import csv
 import re
 
 
-def preproc():
-    pass
+def preproc(file):
+
+    # compile regex
+
+    
+    # open file
+    preprocFile = open(file)
+    str1 = preprocFile.readline()
+    #print(str1)  
+
+    str1 = re.sub('/\s\s+/g,', '', str1)    
+    str1 = re.sub(r'[^a-zA-Z_0-9 ]', '', str1)
+    
+   
+
+    print(str1)
+
+    #bruh i give up idk how to do regex
+    
+    # remove all words that are not white space
+
+
+
 
 def calcTFIDF():
     pass
 
-
 def main():
-    pass
+    
+    preproc('test1.txt')
+
+
+
 main()
 
