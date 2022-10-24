9b7f573a4af4a62a0df65afa334144b7c7be6d12
zainchalisa <zain.chalisa@gmail.com>
2022-10-24 00:39:53 -0400

    fixed document there is a error in removing characters

--- Project 2/tfidf.py
+++ Project 2/tfidf.py
@@ -5,57 +5,49 @@ import re
 
 
 def preproc(file):
-    
-      # compile regex
 
-    
     # open file
     preprocFile = open(file)
-    str1 = preprocFile.readline()
-    print(str1)  
+    preprocFileW = open('preproc.txt', 'w')
+    for line in preprocFile:
 
-    str1 = re.sub(r'http://\S+ | https://\S+', '', str1)
-    str1 = re.sub("[\W]{1,}",' ',str1) 
-    str1 = re.sub('(\\b[A-Za-z] \\b|\\b [A-Za-z]\\b)', '', str1)
-    str1 = str1.lower()
+        str1 = line  
+    
+    # clean up the string
 
+        str1 = re.sub(r'http://\S+ | https://\S+', '', str1)
+        str1 = re.sub("[\W]{1,}",' ',str1) 
+        str1 = re.sub('(\\b[A-Za-z] \\b|\\b [A-Za-z]\\b)', '', str1)
+        str1 = str1.lower()
 
     # get rid of stopword
-    stopList = []
-    stopWords = open('stopwords.txt')
-    for line in stopWords:
-        stopList.append(line.strip())
+
+        stopList = []
+        stopWords = open('stopwords.txt')
+        for line in stopWords:
+            stopList.append(line.strip())
+
+        for stopWord in stopList:
+            str1 = re.sub(r'\b'+ stopWord + r'\b\s+', '', str1)
+
+    # remove suffixes which have ly, ing, ment        
+
+        suffixList = ['ly', 'ing', 'ment']
+
+        for suffix in suffixList:
+            str1 = re.sub(suffix + r'\b', '', str1)
+
+        preprocFileW.write(str1)
     
-
-    for stopWord in stopList:
-        str1 = re.sub(r'\b'+ stopWord + r'\b\s+', '', str1)
-
-    suffixList = ['ly', 'ing', 'ment']
-
-    for suffix in suffixList:
-        str1 = re.sub(suffix + r'\b', '', str1)
-
-    
-
-    
-       
-    print(str1)
-    
-
-    #bruh i give up idk how to do regex
-    
-    # remove all words that are not white space
-
-
-
-
+ 
 def calcTFIDF():
     pass
 
 def main():
     
-    preproc('test1.txt')
-
+    with open('tfidf_docs.txt') as docs:
+        for line in docs:
+            preproc(line.strip())
 
 
 main()
