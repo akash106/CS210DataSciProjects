f4136983cd6e2a6c00e477e8230cbc576cb7c141
akash121801 <ass209@scarletmail.rutgers.edu>
2022-10-25 17:52:49 -0400

    Worked TF-IDf

--- Project 2/tfidf.py
+++ Project 2/tfidf.py
@@ -3,7 +3,10 @@ import collections
 import csv
 import re
 
-
+word_freq = {}
+idf_dict = {}
+idf_calc = {}
+doc_freq = {}
 def preproc(file):
 
     # open file
@@ -47,7 +50,6 @@ def calcTFIDF(file):
     tf_idf = open(file)
     tf_idfW = open(f'tf_idf{file}', 'w')
 
-    word_freq = {}
 
     for line in tf_idf:
         wordList = line.split(' ')
@@ -56,23 +58,58 @@ def calcTFIDF(file):
             if i not in word_freq.keys():
                 totalWords += 1
                 word_freq[i] = 0
-            word_freq[i] += 1
-    print(totalWords)
+            else:
+                word_freq[i] += 1
+            
+    
+   
     print(word_freq) #Calculating frequencies of every word
+    print(doc_freq)
     tf_dict = {}
     
     for i in word_freq.keys():
         tf_dict[i] = word_freq[i] / totalWords
    
     print(tf_dict)    
+    
+def IDFDictFinder(file):
+    
+    IDFRead = open(file)
 
+    for line in IDFRead:
+        wordList = line.split(" ")
+        for i in wordList:
+            if i not in idf_dict.keys():
+                idf_dict[i] = 0
+
+    IDFRead.close()
+
+    IDFRead2 = open(file)            
+    for line in IDFRead2:
+        for key in idf_dict.keys():
+            if re.search(r'\b + key + \b', line):
+                idf_dict[key] += 1
+
+    print(idf_dict)
+
+def calcuIDF(numberOfDocs):
+    print(idf_dict)
+    for key in idf_dict.keys():
+        if key not in idf_calc:
+            idf_calc[key] = (math.log(numberOfDocs/idf_dict[key])) + 1
+    print(idf_calc)    
 
 
 def main():
     with open('tfidf_docs.txt') as docs:
-        for line in docs:
+        list = docs.readlines()
+        
+        for line in list:
             preproc(line.strip())
             calcTFIDF(f'preproc_{line.strip()}')
-
+            IDFDictFinder(f'preproc_{line.strip()}')
+    
+    numberOfDocs = len(list)
+    calcuIDF(numberOfDocs)
 
 main()
